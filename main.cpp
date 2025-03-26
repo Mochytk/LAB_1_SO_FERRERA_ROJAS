ab9102a88976ca94790ae25b46fc9a6d08763bc1
FerreraYera <martin.ferrera@usm.cl>
2025-03-26 17:37:05 -0300

    ya te dije lo que hice

--- main.cpp
+++ main.cpp
@@ -15,50 +15,64 @@ void crear_output(){
     }
 
     //crear carpeta certamenes
-    path carpeta = "Output/Certamenes";
+    carpeta = "Output/Certamenes";
     if (!exists(carpeta)) {
         create_directory(carpeta); 
     }
 
     //crear carpeta controles
+    carpeta = "Output/Controles";
+    if (!exists(carpeta)) {
+        create_directory(carpeta); 
+    }
+
     //crear carpeta tareas
+    carpeta = "Output/Tareas";
+    if (!exists(carpeta)) {
+        create_directory(carpeta); 
+    }
+
     //crear carpeta archivos corruptos
+    carpeta = "Output/Archivos Corruptos";
+    if (!exists(carpeta)) {
+        create_directory(carpeta); 
+    }
 }
 
+void procesar_linea(string linea, string* tipo, string* numero, string* semestre){
+    if (linea.find("tipo:")){
+        //if (linea.find("certamen")) tipo -> "certamen";
+        //else if (linea.find("control")) tipo -> "control";
+        //else if (linea.find("tarea")) tipo -> "tarea";
+    }
+    else if (linea.find("numero:")){
 
-//void dividir_archivo(filesystem archivo){
-//    ifstream archivo(archivo.path());  // Abrir el archivo directamente
-//    string linea;
-//    string tipo;
-//    string numero;
-//    string semestre;
-//    while (getline(archivo, linea)) {  // Leer todas las líneas
-//        procesar_linea(linea);
-//    }
-//    if (tipo == NULL || numero == NULL || semestre == NULL){
-//
-//    }
-//}
+    }
+    else if (linea.find("semestre_publicacion:")){
 
-//void procesar_linea(string linea){
-//    if (linea.find("tipo:")){
-//
-//    }
-//    else if (linea.find("numero:")){
-//
-//    }
-//    else if (linea.find("semestre_publicacion:")){
-//
-//    }
-//}
+    }
+}
 
+void trabajar_archivo(ifstream &archivo){
+    string linea;
+    string* tipo;
+    string* numero;
+    string* semestre;
+    while (getline(archivo, linea)) {  // Leer todas las líneas
+        procesar_linea(linea, tipo, numero, semestre);
+    }
+    if (*tipo == "" || *numero == "" || *semestre == ""){
+
+    }
+}
 
 int main(){
     crear_output();
 
     // Mostrar contenidos carpeta
-    for (const auto &archivo : directory_iterator("General")) {
-        //dividir_archivo(archivo);
+    for (const auto &entrada : directory_iterator("General")) {
+        ifstream archivo(entrada.path());  // Abrir el archivo
+        trabajar_archivo(archivo);
     }
     return 0;
 }
