a11b8d5e7514dd8ef0fd7792cdb8649d4f14eedc
FerreraYera <martin.ferrera@usm.cl>
2025-03-27 19:58:57 -0300

    termine mi parte
    me falta comentar, pero te agregue comentarios de lo que te toca hacer, que es lo de cambiar los nombres de los archivos y moverlos. Deje los 2 funciones para esto creadas pero vacias. Igual te explicare todo esto por audio de ig.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
+
+
 #include <iostream>
 #include <filesystem>
 #include <fstream>
 
 using namespace std;
 using namespace std::filesystem;
-namespace fs = std::filesystem;
 
 void crear_output(){
     //crear carpeta output
@@ -38,81 +39,72 @@ void crear_output(){
     }
 }
 
-void procesar_linea(string linea, string* tipo, string* numero, string* semestre){
-    if (linea.find("tipo:")){
-        //if (linea.find("certamen")) tipo -> "certamen";
-        //else if (linea.find("control")) tipo -> "control";
-        //else if (linea.find("tarea")) tipo -> "tarea";
+void procesar_linea(string linea, string* tipo, string* numero, string* nombre, string* semestre){
+    if (linea.find("tipo:")!= string::npos){
+        if (linea.find("certamen")!= string::npos) *tipo = "certamen";
+        else if (linea.find("control")!= string::npos) *tipo = "control";
+        else if (linea.find("tarea")!= string::npos) *tipo = "tarea";
     }
-    else if (linea.find("numero:")){
+    else if (linea.find("numero:")!= string::npos) *numero = linea.substr(linea.find(":") + 2);
+    else if (linea.find("nombre:")!= string::npos) *nombre = linea.substr(linea.find(":") + 2);
+    else if (linea.find("semestre_publicacion:")!= string::npos) *semestre = linea.substr(linea.find(":") + 2);
+}
 
-    }
-    else if (linea.find("semestre_publicacion:")){
+void mover_corrupto(ifstream &archivo){
+    // aqui si el archivo es corrupto se mueve sin cambiar el nombre
+}
 
+void mover_archivo(ifstream &archivo, string destino){
+    path carpeta = destino;
+    if (!exists(carpeta)) {
+        create_directory(carpeta); 
     }
+    // aqui hay que cambiar el nombre asi que probablemente
+    // necesites el tipo, numero y nombre, dependiendo si es
+    // certamen, control o tarea, asi que habria que agregarlos
+    // como parametros.
+
+    //probablemente lo mejor seria crear una función por cada
+    //tipo de archivo pero me da paja hacerlo. <3
 }
 
 void trabajar_archivo(ifstream &archivo){
     string linea;
-    string* tipo;
-    string* numero;
-    string* semestre;
-    while (getline(archivo, linea)) {  // Leer todas las líneas
-        procesar_linea(linea, tipo, numero, semestre);
+    string tipo;
+    string numero;
+    string nombre;
+    string semestre;
+    string carpeta_destino;
+    while (getline(archivo, linea)) {
+        linea.pop_back();
+        procesar_linea(linea, &tipo, &numero, &nombre, &semestre);
     }
-    if (*tipo == "" || *numero == "" || *semestre == ""){
 
+    if (tipo == "certamen" && numero != "" && semestre != ""){
+        carpeta_destino = "Output/Certamenes/" + semestre;
     }
+    else if (tipo == "control" && numero != "" && semestre != ""){
+        carpeta_destino = "Output/Controles/" + semestre;
+    }
+    else if (tipo == "tarea" && nombre != "" && semestre != ""){
+        carpeta_destino = "Output/Tareas/" + semestre;
+    }
+    else{
+        carpeta_destino = "Output/Archivos Corruptos";
+    }
+
+    if (carpeta_destino == "Output/Archivos Corruptos") mover_corrupto(archivo);
+    else mover_archivo(archivo, carpeta_destino);
+
 }
 
 int main(){
-    
     crear_output();
-    // Mostrar contenidos carpeta
-    for (const auto &entrada : directory_iterator("General")) {
-        ifstream archivo(entrada.path());  // Abrir el archivo
+
+    for (const auto &entrada : directory_iterator("Pruebas/General")) {
+        ifstream archivo(entrada.path());
         trabajar_archivo(archivo);
-    }
-
-    /*
-    Vamos de a poco:
-        - Primero leer la carpeta Pruebas/General
-        - Empezar a leer archivo a archivo
-        - Si encontramos las condiciones de Certamen, Control o Tarea
-          la movemos
-        - Si no, a la carpeta corruptos
-        - y ya.
-    */
-
-    // Ruta de carpeta
-    fs::path carpeta = "Pruebas";
-
-    // Si no existe, crear la carpeta
-    if (!fs::exists(carpeta)) {
-        fs::create_directory(carpeta); 
-        cout << "Carpeta creada: " << carpeta << "\n"; 
-    }
-
-    // Nombre del archivo dentro de la carpeta
-    fs::path ruta_archivo = carpeta / "archivo.txt"; 
-
-    // Crear el archivo
-
-    ofstream archivo(ruta_archivo);
-
-    // Si el archivo se abrió
-    if (archivo.is_open()) {
-        archivo << "probando\n";
         archivo.close();
-        cout << "Archivo creado!" << endl;
-
     }
-    
-    // Si no
-    else {
-        cout << "Error al abrir el archivo!" << endl;
-    }
-
-
     return 0;
 }
